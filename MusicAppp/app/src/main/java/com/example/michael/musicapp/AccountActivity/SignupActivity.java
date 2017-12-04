eeb1e0fe6b8a6c6bcfd302ceebb800817c40fa5c
cs16mab <32703235+cs16mab@users.noreply.github.com>
2017-12-04 23:54:07 +0000

    updated all basic designs

--- MusicAppp/app/src/main/java/com/example/michael/musicapp/AccountActivity/SignupActivity.java
+++ MusicAppp/app/src/main/java/com/example/michael/musicapp/AccountActivity/SignupActivity.java
@@ -1,12 +1,26 @@
 package com.example.michael.musicapp.AccountActivity;
-
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.michael.musicapp.R;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 public class SignupActivity extends AppCompatActivity {
-
+    private EditText inputEmail, inputPassword;     //hit option + enter if you on mac , for windows hit ctrl + enter
+    private Button btnSignIn, btnSignUp, btnResetPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
